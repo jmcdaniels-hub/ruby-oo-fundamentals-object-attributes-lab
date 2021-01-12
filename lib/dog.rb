5a8ef92fb5a37e74076d107958c8327b9a671b6a
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-12 17:02:17 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+
+
+class Dog
+
+    def name=(new_name)
+        @name = new_name
+    end
+
+    def name 
+        @name
+    end 
+
+    def breed=(other_breed)
+        @breed = other_breed
+    end 
+
+    def breed
+        @breed
+    end
+
+end 
+
+
+
+ 
+
